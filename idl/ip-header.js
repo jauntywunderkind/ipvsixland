8630742f2b28e7a4ecad9785f143f6c8e4fbc1f2
rektide de la faye <rektide@voodoowarez.com>
2017-11-05 19:40:03 -0500

    ip-ext.js

---
+++ idl/ip-header.js
@@ -0,0 +1,12 @@
+interface IpHeader {
+	attribute byte version;
+	attribute byte trafficClass;
+	attribute unsigned long flowLabel;
+	attribute unsigned short payloadLength;
+	attribute byte nextHeader;
+	attribute byte hopLimit;
+	attribute long long sourceAddressTop;
+	attribute long long sourceAddressBottom;
+	attribute long long destinationAddressTop;
+	attribute long long destinationAddressBottom;
+};
